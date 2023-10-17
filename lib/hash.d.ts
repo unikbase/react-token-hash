083a5dfd603efdaf65149c2c3f8fcf85caa6dbb2
long.phan <long.phan@netpower.no>
2023-10-18 00:20:04 +0700

    Update compiled script
    https://github.com/unikbase/mobileapp/issues/805

--- lib/hash.d.ts
+++ lib/hash.d.ts
@@ -1,3 +1,14 @@
 import 'json-canonicalize/src/global';
+export declare const randomSalt: () => string;
+export declare const toUint8Array: (input: string) => Uint8Array;
+export declare const uint8ArrayToString: (uint8Buffer: Uint8Array) => string;
+export declare const multihashesSha1: (input: string) => string;
+export declare const decodeMultihash: (hexString: string) => {
+    raw: string;
+    code: import("multihashes/dist/src/constants").HashCode;
+    name: import("multihashes/dist/src/constants").HashName;
+    length: number;
+    digest: Uint8Array;
+};
 export declare const generateJsonHash: (token: any) => string;
 export declare const generateZipHash: (token: any) => string;
