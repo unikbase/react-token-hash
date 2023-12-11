e2a7cb153722a2c37183e51bc5651cc93b39e9cf
long.phan <dragons3232@gmail.com>
2023-12-11 14:51:52 +0700

    Update version

--- lib/hash.d.ts
+++ lib/hash.d.ts
@@ -15,7 +15,7 @@ export declare const generateSalts: (tokenDetails: any) => {
 }[];
 export declare const generateJsonHash: (token: any) => string | null;
 export declare const generateZipHash: (token: any) => string;
-export declare const generateVerifiablePresentation: (token: any, sharedProps: Array<string>, chainId: string, walletAddress: string, sign: (data: any) => Promise<string>) => Promise<{
+export declare const generateVerifiablePresentation: (credentialId: string, token: any, sharedProps: Array<string>, chainId: string, walletAddress: string, sign: (data: any) => Promise<string>) => Promise<{
     proof: {
         type: string;
         created: string;
@@ -23,6 +23,7 @@ export declare const generateVerifiablePresentation: (token: any, sharedProps: A
         verificationMethod: string;
         signature: string;
     };
+    credentialId: string;
     tokenId: any;
     selectiveObjectData: any;
 } | null>;
